8522494e65031f004bfd4a852f28d9dc27a0d636
rt4914 <rajattalesra4914@gmail.com>
2024-03-30 22:49:07 +0530

    search form client side

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -7,8 +7,6 @@ export default class extends Controller {
 
   toggleModal(){
     const modalTriggerId = this.element.dataset.modalTriggerId;
-    console.log("modalTriggerId: ", modalTriggerId)
-    console.log("wrapper: " + document.getElementById(`modal-${modalTriggerId}-wrapper`))
     toggle(document.getElementById(`modal-${modalTriggerId}-wrapper`));
   }
 }
